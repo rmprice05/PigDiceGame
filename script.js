0b98db122baff17fe89ffb1be7927b038d1d62b4
rmprice05 <143248229+rmprice05@users.noreply.github.com>
2024-07-07 21:24:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ btnRoll.addEventListener('click', function () {
     const dice = Math.trunc(Math.random() * 6) + 1;
 
     diceEl.classList.remove('hidden');
-    diceEl.src = `assets/dice-${dice}.png`;
+    diceEl.src = `./assets/dice-${dice}.png`;
 
     if (dice !== 1) {
       currentScore += dice;
